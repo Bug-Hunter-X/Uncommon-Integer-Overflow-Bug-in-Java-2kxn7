075779bc11da1a161f96d727a3fb4cc7258d58a4
BugHunter-X <askzenai@gmail.com>
2025-01-15 16:32:06 +0100

    Add Bug.java file

---
+++ Bug.java
@@ -0,0 +1,17 @@
+public class MyClass {
+    public static void main(String[] args) {
+        int i = 10;
+        try {
+            while (true) {
+                i++;
+                if (i > 1000000) {
+                    System.out.println("i is greater than 1000000");
+                    throw new Exception("Throwing exception");
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Exception caught");
+            System.out.println("i = " + i);
+        }
+    }
+}
